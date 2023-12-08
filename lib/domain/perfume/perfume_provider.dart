40752a1781134f34204f622b38b1d7dccff73192
leonaxdo <32193248@dankook.ac.kr>
2023-12-08 17:04:26 +0900

    상세화면 구현, perfumedetail작성 및 api 연동

--- lib/domain/perfume/perfume_provider.dart
+++ lib/domain/perfume/perfume_provider.dart
@@ -1,37 +1,26 @@
-// perfume_provider.dart
 import 'dart:async';
-import 'package:taba/domain/perfume/perfume.dart';
-import 'package:taba/domain/auth/repository.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:taba/domain/perfume/perfume.dart';
 
-final perfumeListProvider =
-AsyncNotifierProvider<PerfumeListNotifier, PerfumeList>(
-        () => PerfumeListNotifier());
+import '../../routes/router_path.dart';
+import '../../routes/router_provider.dart';
+import '../repository.dart';
 
-class PerfumeListNotifier extends AsyncNotifier<PerfumeList> {
+final perfumeProvider =
+    AsyncNotifierProvider<PerfumeNotifier, PerfumeDetail>(() => PerfumeNotifier());
 
-  final List<Perfume> favoritePerfumeList = [];
-
-  void addFavoritePerfume(Perfume perfume) {
-    favoritePerfumeList.add(perfume);
-  }
-
-  void removeFavoritePerfume(Perfume perfume) {
-    favoritePerfumeList.remove(perfume);
-  }
-
+class PerfumeNotifier extends AsyncNotifier<PerfumeDetail> {
   // 페이지 번호와 한 페이지당 항목 수를 매개변수로 받는 getPerfumeList 함수
-  void getPerfumeList(int page, int size) async {
+  void getPerfumeList(int id) async {
     state = await AsyncValue.guard(
-            () => ref.read(repositoryProvider).getPerfumeList(page, size));
+        () => ref.read(repositoryProvider).getPerfumeDetail(id));
   }
 
   // build 메서드에서 첫 페이지의 데이터를 가져옵니다.
   // 이 예시에서는 첫 페이지를 0으로 가정하고, 한 페이지당 10개의 항목을 가져옵니다.
   @override
-  FutureOr<PerfumeList> build() async {
-    getPerfumeList(0, 10); // 첫 페이지 데이터 로드
-    print('error: ${state.error}');
+  FutureOr<PerfumeDetail> build() async {
     return state.value!;
   }
 }
---
+++ lib/domain/repository.dart
@@ -0,0 +1,119 @@
+// repository.dart
+import 'package:image_picker/image_picker.dart';
+import 'package:taba/domain/perfume/perfume.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../utils/dio_provider.dart';
+import 'auth/login/login.dart';
+
+final repositoryProvider = Provider<Repository>((ref) => Repository(ref.read(dioProvider)));
+
+class Repository {
+
+  final Dio _dio;
+  Repository(this._dio);
+
+  Future<String> getSignupToken() async {
+    final response = await _dio.get('/user/signup-token');
+    return response.data['signupToken'];
+  }
+
+  Future<bool> sendSMS({required String phoneNumber, required String signupToken}) async {
+    final response = await _dio.post(
+      '/sms/$signupToken',
+      data: {
+        'phoneNumber': phoneNumber,
+      },
+    );
+    return response.statusCode == 200;
+  }
+
+  Future<bool> verifySMS({required String code, required String signupToken}) async {
+    final response = await _dio.post(
+      '/sms/verify/$signupToken',
+      data: {
+        'code': code,
+      },
+    );
+    return response.statusCode == 200;
+  }
+
+  Future<Login> login(String nickname, String password) async {
+    final response = await _dio.post(
+      '/user/login',
+      data: {
+        'nickname': nickname,
+        'password': password,
+      },
+    );
+    return Login.fromJson(response.data);
+  }
+
+  Future<bool> signUp({
+    required String signupToken,
+    required String name,
+    required String nickname,
+    required String age,
+    required String gender,
+    required String phone,
+    required String password,
+  }) async {
+    final response = await _dio.post(
+      '/user/$signupToken',
+      data: {
+        'name': name,
+        'nickname': nickname,
+        'age' : age,
+        'gender' : gender,
+        'phone' : phone,
+        'password': password,
+      },
+    );
+    return response.statusCode == 200;
+  }
+
+  Future<bool> verifyNickname({required String nickname}) async {
+    final response = await _dio.post(
+      '/user/signup/verify/$nickname',
+    );
+    return response.statusCode == 200;
+  }
+
+  Future<PerfumeList> getPerfumeList(int page, int size) async {
+    final response = await _dio.get(
+      '/perfume?page=$page&size=$size&sort=rating,desc',
+    );
+    if (response.statusCode == 200) {
+      return PerfumeList.fromJson(response.data);
+    } else {
+      throw Exception('Failed to load perfumes');
+    }
+  }
+
+  Future<PerfumeList> getRecommendedPerfumeList(List<XFile> images) async{
+    final files = images.map((e) => MultipartFile.fromFileSync(
+      e.path,
+    )).toList();
+    final response = await _dio.post(
+      '/user/upload/image-test?page=0&size=20',
+      data: FormData.fromMap({
+        'images': files,
+      }),
+      options: Options(
+        contentType: 'multipart/form-data',
+        headers: {
+          'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyUm9sZSI6IlJPTEVfVVNFUiIsImV4cCI6MTcwMjAyMzUyMywidXNlcklkIjoiMTYiLCJpYXQiOjE3MDIwMjE3MjN9.zBVqMAetLsh95buHrsenYQzeLF1gybIK_ZBztI7A9bY'
+        },
+      ),
+    );
+    return PerfumeList.fromJson(response.data);
+  }
+
+  Future<PerfumeDetail> getPerfumeDetail(int id) async{
+    final response = await _dio.post(
+      '/perfume/$id',
+    );
+    return PerfumeDetail.fromJson(response.data);
+  }
+}
